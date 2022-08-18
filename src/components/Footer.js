8eb9c59b926e9fd455246f940fdbf5373040fc98
ignacioborraz <ignacioborraz@hotmail.com>
2022-08-18 15:54:07 -0300

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+export default function Footer() {
+  return (
+    <div className='Footer-container'>
+        <p className='Footer-p'>amazing events</p>
+        <p className='Footer-p'>borraz - lopez - rodriguez - villafañe</p>
+    </div>
+  )
+}
+
+//rfc
+//react
+//functional
+//component
