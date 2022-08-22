11ce241fbbfee301e57fe191667346e224195c3f
ignacioborraz <ignacioborraz@hotmail.com>
2022-08-22 15:24:30 -0300

    slide's property

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,6 +4,7 @@ import {useEffect, useState} from 'react'
 
 function Carousel(props) {
     const range = props.range
+    const limitSlides = (props.slides * range)
     const [start, setStart] = useState(0)
     const [end, setEnd] = useState(start + range)
     const [intervalId, setIntervalId] = useState()
@@ -29,26 +30,34 @@ function Carousel(props) {
 
     function previous() {
         if (start >= range) {
-            setStart( start-range )
-            setEnd( end-range )
+            setStart(start-range)
+            setEnd(end-range)
+        } else {
+            setStart(limitSlides-range)
+            setEnd(limitSlides)
         }
     }
 
     function next() {
-        if (end < data.length) {
-            setStart( start+range )
-            setEnd( end+range )
+        if (start < limitSlides-range) {
+            setStart(start+range)
+            setEnd(end+range)
+        } else {
+            setStart(0)
+            setEnd(range)
         }
     }
 
     return (
             <div className="Carousel-container">
                 <h4 className="Carousel-title">{props.text}</h4>
-                <div className="Carousel-slide">
+                <div className="Carousel-full">
                     <Arrow icon={"<"} click={previous} />
-                    {data.slice(start, end).map(events)}
+                    <div className="Carousel-slide">
+                        {data.slice(start, end).map(events)}
+                    </div>
                     <Arrow icon={">"} click={next} />
-                </div>
+                </div>               
             </div>
     )
 }
