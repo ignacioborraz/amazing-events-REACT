0dbfdbfe2efa683c9157bf95c350768c891a9c83
Eric Rodriguez <feric.rodriguez@gmail.com>
2022-08-23 18:28:27 -0300

    Scroll up button created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,24 @@
 import React from 'react'
-//import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import '../styles/Footer.css'
 
 
-
 export default function Footer() {
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
   return (
     <div className='Footer-container'>
         <p className='Footer-title'>Amazing Events</p>
         <p className='Footer-p'>borraz - lopez - rodriguez - villafañe</p>
+        <button onClick={scrollUp}>TOP</button>
+        <Link to='/'>Home</Link>
+        <Link to='/events'>Events</Link>
     </div>
   )
 }
