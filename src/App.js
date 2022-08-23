e08516716e7a87120b4728b5e16fe8752f01b236
Eric Rodriguez <feric.rodriguez@gmail.com>
2022-08-23 11:38:47 -0300

    Solution react router context error

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import HomePage from './pages/HomePage'
 import EventsPage from './pages/EventsPage'
 import UnderConstruction from './pages/UnderConstruction'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import WebsiteLayout from './layouts/WebsiteLayout'
 
 
 function App() {
   return (
-    <>
       <BrowserRouter>
+      <WebsiteLayout>
         <Routes>
           <Route path='/' element={<HomePage />}/>
           <Route path='/events' element={<EventsPage />}/>
-          {/* <UnderConstruction /> */}
+          <Route path='*' element={<UnderConstruction />}/>
         </Routes>
+      </WebsiteLayout>      
       </BrowserRouter>
-    </>
+      
   )
 }
 
