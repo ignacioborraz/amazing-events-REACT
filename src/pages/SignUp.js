6efa7274992f3a836741b6977c06d570d2f80ae9
Eric Rodriguez <feric.rodriguez@gmail.com>
2022-09-12 16:55:07 -0300

    Sign Up page created

---
+++ src/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignUpGoogle from '../components/SignUpGoogle'
+
+
+export default function SignUp() {
+  return (
+    <div>
+        <SignUpGoogle />
+        <form action="">
+            <input type="text" />
+        </form>
+    </div>
+  )
+}
