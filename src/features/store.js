7e4d022b5c3522773ad96b0eab7701d82d44d4e1
ignacioborraz <ignacioborraz@hotmail.com>
2022-10-05 20:01:42 -0300

    likes

--- src/features/store.js
+++ src/features/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import eventsAPI from './eventsAPI'
 import authAPI from './authAPI'
+import likesAPI from './likesAPI'
 import authReducer from './authSlice'
 
 export default configureStore({
     reducer: {
         [eventsAPI.reducerPath] : eventsAPI.reducer,
         [authAPI.reducerPath] : authAPI.reducer,
+        [likesAPI.reducerPath] : likesAPI.reducer,
         auth: authReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
